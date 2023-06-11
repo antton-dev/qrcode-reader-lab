c7ae4e509d57465361e71e3de3ab06df48ad2cba
anttondev <antton.chevrier@gmail.com>
2023-06-11 23:45:20 +0200

    add thumbnail if available

--- main.js
+++ main.js
@@ -10,13 +10,14 @@ function onScanSuccess(decodedText, decodedResult) {
     let result = document.querySelector('#result')
     console.log(decodedResult);
 
-    fetch("https://www.googleapis.com/books/v1/volumes?q=isbn:" + decodedText + "&key=AIzaSyDpS7bVi263xStL7x0dlAN_Hr7iToQZsH8")
+    fetch("https://www.googleapis.com/books/v1/volumes?q=" + decodedText + "+isbn&maxResults=1")
+    // fetch("https://www.googleapis.com/books/v1/volumes?q=isbn:" + decodedText + "&key=AIzaSyDpS7bVi263xStL7x0dlAN_Hr7iToQZsH8")
     .then(res => res.json())
     .then(data => {
         console.log(data.items);
         data.items.forEach(item => {
-            console.log(item.volumeInfo.title)
-            result.insertAdjacentHTML('beforeend', item.volumeInfo.title + ' - ' + item.volumeInfo.authors + '<br>')
+            console.log(item.volumeInfo.title, item.volumeInfo.imageLinks.smallThumbnail)
+            result.insertAdjacentHTML('beforeend', "<img src='" + item.volumeInfo.imageLinks.smallThumbnail + "'alt='cover'><br>" + item.volumeInfo.title + ' - ' + item.volumeInfo.authors + '<br>')
 
         });
     })
