71e4194c9cccb88ce4426ce56ea9163997925c18
anttondev <antton.chevrier@gmail.com>
2023-06-11 22:02:23 +0200

    change config

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 var html5QrcodeScanner = new Html5QrcodeScanner(
-    "reader", { fps: 10, qrbox: 250 });
+    "reader", { fps: 10, qrbox: 250, aspectRatio: 1, formatsToSupport: [9]});
         
 function onScanSuccess(decodedText, decodedResult) {
     // Handle on success condition with the decoded text or result.
     html5QrcodeScanner.clear();
     let result = document.querySelector('#result')
     result.insertAdjacentText('beforeend', decodedText)
+    console.log(decodedResult);
 }
 
 html5QrcodeScanner.render(onScanSuccess);
