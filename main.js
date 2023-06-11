6b6fb257ccce8bc92f8bc57a2986ec0633828e50
anttondev <antton.chevrier@gmail.com>
2023-06-11 23:05:59 +0200

    link GoogleBooks API

--- main.js
+++ main.js
@@ -5,8 +5,20 @@ function onScanSuccess(decodedText, decodedResult) {
     // Handle on success condition with the decoded text or result.
     html5QrcodeScanner.clear();
     let result = document.querySelector('#result')
-    result.insertAdjacentText('beforeend', decodedText)
     console.log(decodedResult);
+
+    fetch("https://www.googleapis.com/books/v1/volumes?q=isbn:" + decodedText + "&key=AIzaSyDpS7bVi263xStL7x0dlAN_Hr7iToQZsH8")
+    .then(res => res.json())
+    .then(data => {
+        console.log(data.items);
+        data.items.forEach(item => {
+            console.log(item.volumeInfo.title)
+            result.insertAdjacentHTML('beforeend', item.volumeInfo.title + ' - ' + item.volumeInfo.authors + '<br>')
+
+        });
+    })
 }
 
 html5QrcodeScanner.render(onScanSuccess);
+
+
