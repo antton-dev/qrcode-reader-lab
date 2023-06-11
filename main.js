340c4600dfc83247fd95c0f04ea2de2108cc4ca2
anttondev <antton.chevrier@gmail.com>
2023-06-11 23:27:14 +0200

    add timeout

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-var html5QrcodeScanner = new Html5QrcodeScanner(
+let html5QrcodeScanner = new Html5QrcodeScanner(
     "reader", { fps: 10, qrbox: 250, aspectRatio: 1, formatsToSupport: [9]});
         
 function onScanSuccess(decodedText, decodedResult) {
     // Handle on success condition with the decoded text or result.
-    html5QrcodeScanner.clear();
+    setTimeout(() => {
+        html5QrcodeScanner.clear();
+    }, 1000);
+    
     let result = document.querySelector('#result')
     console.log(decodedResult);
 
